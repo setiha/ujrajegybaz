3f4b07da30947a3a30e86d0699618e5813132cf3
Tihici <tihamer.sebok@gmail.com>
2020-06-03 20:26:49 +0300

    meg tobb komponens generalasa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { EventcardComponent } from './eventcard/eventcard.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EventComponent } from './event/event.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { EventComponent } from './event/event.component';
     EventcardComponent,
     FooterComponent,
     NavbarComponent,
-    EventComponent
+    EventComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ProfileEditComponent
   ],
   imports: [
     BrowserModule,
