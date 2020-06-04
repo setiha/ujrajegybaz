7b0c8e42050dc5c5b1d558e2b364ff8a2791efe7
Tihici <tihamer.sebok@gmail.com>
2020-06-04 17:57:05 +0300

    A komponensek befejezesenek a generalasa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { BidComponent } from './bid/bid.component';
+import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +34,15 @@ import { ProfileEditComponent } from './profile-edit/profile-edit.component';
     LoginComponent,
     RegisterComponent,
     ProfileComponent,
-    ProfileEditComponent
+    ProfileEditComponent,
+    EventDetailComponent,
+    PageNotFoundComponent,
+    AboutComponent,
+    HomeComponent,
+    BidComponent,
+    TicketDetailComponent,
+    TicketListComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
