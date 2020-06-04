4a6bac3820d7f7a22d5b108516b010c6555a2b23
Tihici <tihamer.sebok@gmail.com>
2020-06-04 18:16:20 +0300

    App-routing module, es a home bekotese

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CollapseModule} from "ngx-bootstrap/collapse";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CollapseModule} from 'ngx-bootstrap/collapse';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { EventcardComponent } from './eventcard/eventcard.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { EventComponent } from './event/event.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { BidComponent } from './bid/bid.component';
-import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
-import { TicketListComponent } from './ticket-list/ticket-list.component';
-import { TicketComponent } from './ticket/ticket.component';
+
 
 @NgModule({
   declarations: [
@@ -30,19 +18,7 @@ import { TicketComponent } from './ticket/ticket.component';
     EventcardComponent,
     FooterComponent,
     NavbarComponent,
-    EventComponent,
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent,
-    ProfileEditComponent,
-    EventDetailComponent,
-    PageNotFoundComponent,
-    AboutComponent,
-    HomeComponent,
-    BidComponent,
-    TicketDetailComponent,
-    TicketListComponent,
-    TicketComponent
+    ...AppRoutingModule.routableComponent
   ],
   imports: [
     BrowserModule,
