da323e11b77e259b60f0a464a1a9ed62ef6945a6
Tihici <tihamer.sebok@gmail.com>
2020-06-04 19:19:09 +0300

    App-routing module-navbar komponent a href ek atirva routerLink re,  a path ek elkeszitve AppRoutinModulba behuzva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {EventComponent} from './event/event.component';
+import {TicketComponent} from './ticket/ticket.component';
+import {AboutComponent} from './about/about.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'event', component: EventComponent},
+  {path: 'ticket', component: TicketComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -13,6 +26,12 @@ const routes: Routes = [
 })
 export class AppRoutingModule {
   static routableComponent = [
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent,
+    EventComponent,
+    TicketComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent
   ];
 }
