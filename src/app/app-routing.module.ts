bb7b3065ea4a5058207125cb299774e950b762de
Tihici <tihamer.sebok@gmail.com>
2020-06-08 13:05:11 +0300

    ticket sitebuild bekotes es adaptalas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import {ProfileComponent} from './user/profile/profile.component';
 import {ProfileEditComponent} from './user/profile-edit/profile-edit.component';
 import {TicketListComponent} from "./ticket/ticket-list/ticket-list.component";
 import {TicketDetailComponent} from "./ticket/ticket-detail/ticket-detail.component";
+import {BidComponent} from "./ticket/bid/bid.component";
 
 
 const routes: Routes = [
@@ -30,7 +31,7 @@ const routes: Routes = [
     children: [
       {path: 'list', component: TicketListComponent},
       {path: 'new', component: TicketDetailComponent},
-      {path: ':id/bid', component: TicketDetailComponent}
+      {path: ':id/bid', component: BidComponent}
     ]
   },
   {path: 'about', component: AboutComponent},
@@ -65,6 +66,7 @@ export class AppRoutingModule {
     ProfileEditComponent,
     TicketDetailComponent,
     TicketComponent,
-    TicketListComponent
+    TicketListComponent,
+    BidComponent
   ];
 }
