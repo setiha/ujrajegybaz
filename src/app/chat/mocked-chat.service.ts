f691d52a86f534f0adce46f592e0ad5eca7d0e27
Tihici <tihamer.sebok@gmail.com>
2020-10-17 17:02:56 +0300

    modify create mocked chat service

--- src/app/chat/mocked-chat.service.ts
+++ src/app/chat/mocked-chat.service.ts
@@ -1,23 +1,51 @@
 import { Injectable } from '@angular/core';
 import {ChatService} from "./chat.service";
-import {Observable} from "rxjs/Rx";
+import {BehaviorSubject, Observable} from "rxjs/Rx";
 import {ChatMessageModel} from "./model/chat.model";
 import {UserService} from "../shared/user.service";
+import "rxjs-compat/add/operator/delay";
+import "rxjs-compat/add/observable/of";
 
 @Injectable({
   providedIn: 'root'
 })
-export class MockedChatService extends ChatService{
+export class MockedChatService extends ChatService{private rooms$ = new BehaviorSubject<BehaviorSubject<ChatMessageModel[]>[]>([]);
 
   constructor(userService: UserService) {
     super(userService);
   }
 
   addMessage(roomId: string, msg: string): Observable<boolean> {
-    return super.addMessage(roomId, msg);
+    const rooms = this.rooms$.getValue();
+    const roomMessages = rooms[roomId].getValue();
+    return this.userService.getCurrentUser()
+      .delay(300)
+      .pipe()
+      .switchMap(
+        user => {
+          roomMessages.push(
+            new ChatMessageModel({
+              $id: null,
+              'msg': msg,
+              userId: user.id,
+              userName: user.name,
+              userPictureUrl: user.profilePictureUrl
+
+            })
+          );
+          rooms[roomId].next(roomMessages);
+          return Observable.of(true);
+        }
+      );
   }
 
   getRoomMessages(roomId: string): Observable<ChatMessageModel[]> {
-    return super.getRoomMessages(roomId);
+    const rooms = this.rooms$.getValue();
+    if (rooms[roomId] == null){
+      //first init room
+      rooms[roomId] = new BehaviorSubject<ChatMessageModel[]>([]);
+      this.rooms$.next(rooms);
+    }
+    return rooms[roomId].asObservable();
   }
 }
